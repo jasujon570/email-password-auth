a5d02cf58633887d12db06dbadaf5439aab78e76
sujon <jasujon570@gmail.com>
2025-10-14 23:21:05 +0600

    register page designed

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React from "react";
 
 const Register = () => {
-    return (
-        <div>
-            <h1>This is register page</h1>
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log("register clicked", email, password);
+  };
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="text-center lg:text-left mb-5">
+            <h1 className="text-5xl font-bold">Register now!</h1>
+          </div>
+          <div className="card-body">
+            <form onSubmit={handleRegister}>
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input
+                  type="email"
+                  className="input"
+                  placeholder="Email"
+                  name="email"
+                />
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                  name="password"
+                />
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button className="btn btn-neutral mt-4">Register</button>
+              </fieldset>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Register;
