695e3ac5f00632e89b40b0498703b7be49b483b1
sujon <jasujon570@gmail.com>
2025-10-14 22:54:57 +0600

    navbar complete and created some components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 import './index.css'
 import App from './App.jsx'
+import Root from './layout/Root.jsx';
+import Home from './components/Home/Home.jsx';
+import Login from './components/Login/Login.jsx';
+import Register from './components/Register/Register.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home
+      },
+      {
+        path: '/register',
+        Component: Register
+      },
+      {
+        path: '/login',
+        Component: Login
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
